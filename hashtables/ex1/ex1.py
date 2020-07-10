c98f096c480c5f490481b1289eac95a9ecbbdc18
NealWhitlock <NealWhitlock@users.noreply.github.com>
2020-07-10 09:17:52 -0600

    ex1 done and tested

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,16 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # Dict to save the complement of each num
+    comp_dict = {}
+
+    # Iterate through weights
+    for i, num in enumerate(weights):
+        # Check if the complement of num is in the dictionary
+        if (limit-num) in comp_dict:
+            # Return a tuple of indices for the two numbers
+            return (i, comp_dict[limit-num])
+        # If not, save the index of num in the dictionary
+        comp_dict[num] = i
 
     return None
